3f3edf4e3d22d1cf7439b636f52cefdd8ba96e7f
jeffe <jeffe@07974df6-c004-4808-b969-191c302cd71a>
2011-03-04 18:44:53 +0000

    Update auf aktuellen Stand aus vormaliger r56
    git-svn-id: svn://svn.rexdev.de/plugins/image_manager/referrer_block.image_manager.plugin/trunk@3 07974df6-c004-4808-b969-191c302cd71a

--- referrer_block.imagemanager.plugin/config.inc.php
+++ referrer_block.imagemanager.plugin/config.inc.php
@@ -7,6 +7,35 @@
 * $Id$:
 */
 
+// ADDON IDENTIFIER & ROOT DIR
+////////////////////////////////////////////////////////////////////////////////
+$myself = 'referrer_block.imagemanager.plugin';
+$myroot = $REX['INCLUDE_PATH'].'/addons/image_manager/plugins/'.$myself;
+
+
+// REX COMMONS
+////////////////////////////////////////////////////////////////////////////////
+$Revision = '';
+$REX['ADDON'][$myself]['VERSION'] = array
+(
+'VERSION'      => 0,
+'MINORVERSION' => 1,
+'SUBVERSION'   => preg_replace('/[^0-9]/','',"$Revision$")
+);
+$REX['ADDON']['version'][$myself]     = implode('.', $REX['ADDON'][$myself]['VERSION']);
+$REX['ADDON']['title'][$myself]       = 'Referrer Block';
+$REX['ADDON']['author'][$myself]      = 'rexdev.de';
+$REX['ADDON']['supportpage'][$myself] = 'forum.redaxo.de';
+$REX['ADDON']['perm'][$myself]        = $myself.'[]';
+$REX['PERM'][]                        = $myself.'[]';
+
+// DYN SETTINGS
+////////////////////////////////////////////////////////////////////////////////
+// --- DYN
+$REX["ADDON"]["image_manager"]["PLUGIN"]["referrer_block.imagemanager.plugin"]["rex_img_file"] = '';
+$REX["ADDON"]["image_manager"]["PLUGIN"]["referrer_block.imagemanager.plugin"]["rex_img_type"] = '';
+// --- /DYN
+
 // MAIN
 ////////////////////////////////////////////////////////////////////////////////
 if(isset($_SERVER['HTTP_REFERER']) && isset($_GET['rex_img_file']) && isset($_GET['rex_img_type']))
@@ -14,43 +43,27 @@ if(isset($_SERVER['HTTP_REFERER']) && isset($_GET['rex_img_file']) && isset($_GE
   $referrer = parse_url($_SERVER['HTTP_REFERER']);
   if($referrer['host']!=$_SERVER['HTTP_HOST'])
   {
-    rex_register_extension('IMAGE_MANAGER_INIT'     ,'referrer_block_init');
-    rex_register_extension('IMAGE_MANAGER_FILTERSET','referrer_block_filterset');
+    rex_register_extension('IMAGE_MANAGER_INIT','referrer_block_init');
 
-    // CREATE VIRTUAL IMG_TYPE, SET IMG_FILE
     function referrer_block_init($params)
     {
       global $REX;
-      
-      $rex_image_type = $params['subject']['rex_img_type'];
 
-      $REX['ADDON']['image_manager']['PLUGINS']['referrer_block.imagemanager.plugin']['params'] = $params['subject'];
+      $replace_file = $REX["ADDON"]["image_manager"]["PLUGIN"]["referrer_block.imagemanager.plugin"]["rex_img_file"];
+      $replace_type = $REX["ADDON"]["image_manager"]["PLUGIN"]["referrer_block.imagemanager.plugin"]["rex_img_type"];
 
-      $params['subject']['rex_img_type'] = 'REFBLOCK'.$rex_image_type;
+      if($replace_file!='')
+      {
+        $params['subject']['rex_img_file'] = $replace_file;
+        $params['subject']['imagepath'] = $REX['HTDOCS_PATH'].'/files/'.$replace_file;
+      }
+
+      if($replace_type!='')
+      {
+        $params['subject']['rex_img_type'] = $replace_type;
+      }
 
       return $params['subject'];
-    }
-
-    // VIRTUAL FILTER SET
-    function referrer_block_filterset()
-    {
-      global $REX;
-      $params= $REX['ADDON']['image_manager']['PLUGINS']['referrer_block.imagemanager.plugin']['params'];
-
-      // ORIGINAL IMG_TYPE FILTERSET
-      $imagepath = $params['imagepath'];
-      $cachepath = $params['cachepath'];
-      $tmp         = new rex_image($imagepath);
-      $tmp_cacher  = new rex_image_cacher($cachepath);
-      $tmp_manager = new rex_image_manager($tmp_cacher);
-      $set = $tmp_manager->effectsFromType($params['rex_img_type']);
-      unset($tmp,$tmp_cacher,$tmp_manager);
-
-      $my_set   = array(); 
-      $my_set[] = array('effect' => 'filter_greyscale','params' => array());
-      $my_set[] = array('effect' => 'filter_blur','params'    => array('amount'=>80,'radius'=>8,'threshold'=>3));
-      $set = array_merge_recursive($set,$my_set);
-      return $set;
     }
   }
 }
