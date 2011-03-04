609ae3db5ca1952185faead42f72a2de77efd1e1
jeffe <jeffe@17db8f5d-9d4b-4f55-aa49-a5b5ca7460e6>
2011-03-04 18:47:09 +0000

    Update auf aktuellen Stand aus vormaliger r56
    git-svn-id: svn://svn.rexdev.de/plugins/image_manager/rex_img_dir.image_manager.plugin/trunk@3 17db8f5d-9d4b-4f55-aa49-a5b5ca7460e6

--- rex_img_dir.imagemanager.plugin/config.inc.php
+++ rex_img_dir.imagemanager.plugin/config.inc.php
@@ -7,6 +7,38 @@
 * $Id$:
 */
 
+// ADDON IDENTIFIER & ROOT DIR
+////////////////////////////////////////////////////////////////////////////////
+$myself = 'rex_img_dir.imagemanager.plugin';
+$myroot = $REX['INCLUDE_PATH'].'/addons/image_manager/plugins/'.$myself;
+
+
+// REX COMMONS
+////////////////////////////////////////////////////////////////////////////////
+$Revision = '';
+$REX['ADDON'][$myself]['VERSION'] = array
+(
+'VERSION'      => 0,
+'MINORVERSION' => 1,
+'SUBVERSION'   => preg_replace('/[^0-9]/','',"$Revision$")
+);
+$REX['ADDON']['version'][$myself]     = implode('.', $REX['ADDON'][$myself]['VERSION']);
+$REX['ADDON']['title'][$myself]       = 'Image Directory';
+$REX['ADDON']['author'][$myself]      = 'rexdev.de';
+$REX['ADDON']['supportpage'][$myself] = 'forum.redaxo.de';
+$REX['ADDON']['perm'][$myself]        = $myself.'[]';
+$REX['PERM'][]                        = $myself.'[]';
+
+
+// DYN SETTINGS
+////////////////////////////////////////////////////////////////////////////////
+// --- DYN
+$REX["ADDON"]["image_manager"]["PLUGIN"]["rex_img_dir.imagemanager.plugin"]["img_dirs"] = array (
+  1 => 'files/addons/image_manager/plugins/rex_img_dir.imagemanager.plugin',
+);
+// --- /DYN
+
+
 // MAIN
 ////////////////////////////////////////////////////////////////////////////////
 if(isset($_GET['rex_img_dir']))
@@ -17,17 +49,15 @@ if(isset($_GET['rex_img_dir']))
   {
     global $REX;
 
-    $rex_img_dir = rex_get('rex_img_dir','string');
-    
-    // SANITIZE USER INPUT
-    $rex_img_dir = preg_replace('/[^a-zA-Z0-9\/-_]/','',$rex_img_dir);
-    $rex_img_dir = trim($rex_img_dir,'/');
-    $rex_img_dir = array_diff(explode('/',$rex_img_dir),array('redaxo','..'));
-    $rex_img_dir = implode('/',$rex_img_dir);
+    $img_dir_id  = rex_get('rex_img_dir','int');
+    $img_dirs    = $REX['ADDON']['image_manager']['PLUGIN']['rex_img_dir.imagemanager.plugin']['img_dirs'];
 
-    $imagepath = $params['subject']['imagepath'];
-
-    $params['subject']['imagepath'] = str_replace('files',$rex_img_dir,$imagepath);
+    if(isset($img_dirs[$img_dir_id]))
+    {
+      $newpath   = $img_dirs[$img_dir_id];
+      $oldpath   = $params['subject']['imagepath'];
+      $params['subject']['imagepath'] = str_replace('files',$newpath,$oldpath);
+    }
 
     return $params['subject'];
   }
