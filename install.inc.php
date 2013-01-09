02706405e460f826f8d201ee2f4dd566cd3e8b07
jdlx <github@rexdev.de>
2013-01-09 03:57:56 +0100

    add folder name install check..

--- install.inc.php
+++ install.inc.php
@@ -13,6 +13,19 @@
  * @version 1.2.2
  */
 
+$myself = 'image_manager';
+
+// CHECK ADDON FOLDER NAME
+////////////////////////////////////////////////////////////////////////////////
+$addon_folder = basename(dirname(__FILE__));
+if($addon_folder != $myself)
+{
+  $REX['ADDON']['installmsg'][$addon_folder] = '<br />Der Name des Addon-Ordners ist inkorrekt: <code style="color:black;font-size:12px;">'.$addon_folder.'</code>
+                                                <br />Addon-Ordner in <code style="color:black;font-size:1.23em;">'.$myself.'</code> umbenennen und Installation wiederholen';
+  $REX['ADDON']['install'][$addon_folder] = 0;
+  return;
+}
+
 $error = '';
 
 if (!extension_loaded('gd'))
